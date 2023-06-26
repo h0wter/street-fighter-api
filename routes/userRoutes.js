596c47cf8e4d8d1e84a16727b3b6ab20d9359627
h0wter <91832203+h0wter@users.noreply.github.com>
2023-06-27 02:34:44 +0300

    add: all user routes
    GET /api/users
    GET /api/users/:id
    POST /api/users
    PUT /api/users/:id
    DELETE /api/users/:id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,108 @@ import {
   updateUserValid,
 } from "../middlewares/user.validation.middleware.js";
 import { responseMiddleware } from "../middlewares/response.middleware.js";
+import { RequestError } from "../helpers/RequestError.js";
 
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get(
+  "/",
+  (_, res, next) => {
+    try {
+      const data = userService.getAll();
+      res.locals.data = data;
+    } catch (error) {
+      res.locals.err = error;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.get(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const data = userService.getUserById(req.params.id);
+      if (data) {
+        res.locals.data = data;
+      } else {
+        throw RequestError(404, "User not found");
+      }
+    } catch (error) {
+      res.locals.err = error;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.post(
+  "/",
+  createUserValid,
+  (req, res, next) => {
+    if (res.locals.err) {
+      next();
+      return;
+    }
+
+    try {
+      const data = userService.addUser(req.body);
+      res.locals.data = data;
+    } catch (error) {
+      res.locals.err = error;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.put(
+  "/:id",
+  updateUserValid,
+  (req, res, next) => {
+    if (res.locals.err) {
+      next();
+      return;
+    }
+
+    try {
+      const data = userService.updateUserById(req.params.id, req.body);
+      if (data) {
+        res.locals.data = data;
+      } else {
+        throw RequestError(404, "User not found");
+      }
+    } catch (error) {
+      res.locals.err = error;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    try {
+      const data = userService.deleteUserById(req.params.id, req.body);
+      if (data) {
+        res.locals.data = data;
+      } else {
+        throw RequestError(404, "User not found");
+      }
+    } catch (error) {
+      res.locals.err = error;
+    } finally {
+      next();
+    }
+  },
+  responseMiddleware
+);
 
 export { router };
