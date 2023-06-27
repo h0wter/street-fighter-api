6570a4a4a000d82b7d6e68200366203edbe2ec41
h0wter <91832203+h0wter@users.noreply.github.com>
2023-06-27 04:16:28 +0300

    update: adds if-else statement for data

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,15 @@ router.post(
   (req, res, next) => {
     try {
       // TODO: Implement login action (get the user if it exist with entered credentials)
-      res.locals.data = data;
+      const data = authService.login(req.body);
+      if (data) {
+        res.locals.data = data;
+      } else {
+        throw RequestError(
+          404,
+          "The user with such credentials does not exist."
+        );
+      }
     } catch (err) {
       res.locals.err = err;
     } finally {
