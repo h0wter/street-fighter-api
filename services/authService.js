688f4576f20253fcfa720219d110da2c41bdddd4
h0wter <91832203+h0wter@users.noreply.github.com>
2023-06-27 04:17:37 +0300

    update: changes the way errors are generated

--- services/authService.js
+++ services/authService.js
@@ -1,10 +1,11 @@
 import { userService } from "./userService.js";
+import { RequestError } from "../helpers/RequestError.js";
 
 class AuthService {
   login(userData) {
     const user = userService.search(userData);
     if (!user) {
-      throw Error("User not found");
+      throw RequestError(404, "User not found");
     }
     return user;
   }
